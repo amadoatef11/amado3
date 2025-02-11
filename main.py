d8419834f826639fa8aa6db5f3d5afcd183747ae
amadoatef11 <amadoatef11@gmail.com>
2025-02-11 18:59:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from flet import *
+import sys
+import codecs
+
+# Ensure your encoding is set to 'utf-8'
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+
+# Your code logic here
 
 def main(page:Page):
     T = Text('Rakwan ali')
